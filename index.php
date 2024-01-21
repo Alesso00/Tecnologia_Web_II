86548bbb3e9192fec9ac8fd7e8574f737801ba90
Alesso00 <danyrosam@gmail.com>
2024-01-21 11:31:40 -0600

    Inicio de clases Univo

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/styles_index.css">
+    <title>Alessandro</title>
+</head>
+<body>
+    
+    <form action="" method="POST" role="form">
+        <legend>Informacion personal</legend>
+    
+        <div class="form-group">
+            <label for="">Nombre</label>
+            <input type="text" class="form-control" id="" placeholder="Input field">
+        </div>
+
+        <div class="form-group">
+            <label for="">Apellido</label>
+            <input type="text" class="form-control" id="" placeholder="Input field">
+        </div>
+    
+        <div class="form-group">
+            <label for="">Edad</label>
+            <input type="text" class="form-control" id="" placeholder="Input field">
+        </div>
+        
+        <div class="form-group">
+            <label for="">Direccion</label>
+            <input type="text" class="form-control" id="" placeholder="Input field">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+    
+</body>
+</html>
